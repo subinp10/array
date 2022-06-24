196f84639f7e6d906b2e58b57bf9c4dbec20fbd5
subinp10 <108092532+subinp10@users.noreply.github.com>
2022-06-24 10:30:02 +0530

    Update order.cpp

--- order.cpp
+++ order.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 using namespace std;
-
 int main() {
 	int b,n,i,t,k;
 		n=5;
 		int a[n];
 	    cin>>b;
-		cin>>n;
-		cin>>k;
+		
+		
 		for(i=0;i<b;i++)
-		{
+		{ 
+		    cin>>n;
+		    cin>>k;	
 		for(i=0;i<n;i++)
 		 cin>>a[i];
 	    for(i=n-k;i<n;i++)
